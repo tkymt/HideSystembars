8abe336c98a02bddca7dc660867d00c9ddd62511
Taku Yamamoto <jts0ymt@outlook.jp>
2023-12-23 02:21:50 +0900

    Initial commit

---
+++ app/src/main/java/com/example/hidesystembars/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.hidesystembars
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        hideSystemBarsForImmersiveMode()
+    }
+
+    private fun hideSystemBarsForImmersiveMode() {
+        val insetsController = WindowCompat.getInsetsController(window, window.decorView)
+        insetsController.hide(WindowInsetsCompat.Type.systemBars())
+        insetsController.systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+    }
+}
